44347f7f128b7dc6ae1ebf04e7008a39321520bb
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-09-04 10:05:23 +0200

    Initial Commit

---
+++ ScrollView-SwiftUI/ScrollView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollView_SwiftUIApp.swift
+//  ScrollView-SwiftUI
+//
+//  Created by Macbook on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrollView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
